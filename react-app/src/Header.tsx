c8d99d1343ce6cbaca475347714fc8cf72c43dfc
mamaciel <marcospaulofilho@hotmail.com>
2023-06-28 01:33:06 -0400

    Header Fonts Update

--- react-app/src/Header.tsx
+++ react-app/src/Header.tsx
@@ -4,7 +4,7 @@ import logoImage from './assets/Marcos.png';
 const Header = () => {
   return (
     <header className={HeaderCSS.header}>
-      <h1 className={HeaderCSS.h1}>Cash Back Credit Card Comparison Tool</h1>
+      <h1 className={HeaderCSS.h1}>Cash Back Comparison Tool</h1>
       <div className={HeaderCSS.contentContainer}>
         <h3 className={HeaderCSS.h3}>By Marcos Maciel</h3>
         <img src={logoImage} className={HeaderCSS.logo} alt="Logo" />
