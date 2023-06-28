64e1f176a695caac3a72040cc7373fd30a95b86f
mamaciel <marcospaulofilho@hotmail.com>
2023-06-28 00:24:49 -0400

    Initial Commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+import Header from './Header.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/Header.tsx
@@ -0,0 +1,10 @@
+import HeaderCSS from './Header.module.css'
+
+const Header = () => {
+    return(
+        <header className="">
+            <h1 className={HeaderCSS.h1}>Cash Back Credit Card Comparison Tool</h1>
+        </header>
+    )
+}
+export default Header
