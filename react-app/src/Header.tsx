c963835690f80e366bddca676717d3ac8cc3863f
mamaciel <marcospaulofilho@hotmail.com>
2023-06-28 01:17:39 -0400

    Header updated

--- react-app/src/Header.tsx
+++ react-app/src/Header.tsx
@@ -1,10 +1,16 @@
-import HeaderCSS from './Header.module.css'
+import HeaderCSS from './Header.module.css';
+import logoImage from './assets/Marcos.png';
 
 const Header = () => {
-    return(
-        <header className="">
-            <h1 className={HeaderCSS.h1}>Cash Back Credit Card Comparison Tool</h1>
-        </header>
-    )
-}
-export default Header
+  return (
+    <header className={HeaderCSS.header}>
+      <h1 className={HeaderCSS.h1}>Cash Back Credit Card Comparison Tool</h1>
+      <div className={HeaderCSS.contentContainer}>
+        <h3 className={HeaderCSS.h3}>By Marcos Maciel</h3>
+        <img src={logoImage} className={HeaderCSS.logo} alt="Logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
