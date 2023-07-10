cd104567bc05e16e918ade6463994d1cbdefecf6
Marcos Maciel <mamaciel2022@gmail.com>
2023-07-09 23:08:28 -0400

    Fixed table issues

--- react-app/src/ComparisonTable.tsx
+++ react-app/src/ComparisonTable.tsx
@@ -11,89 +11,123 @@ const ComparisonTable = () => {
 
   return (
     <>
-    <h2>Credit Card</h2>
-      
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {cardNames.map((name, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{name}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.tableContainer}>
+        <div className={styles.tableWrapper}>
+          <h2>Credit Card</h2>
 
-      <h2>Annual Fee</h2>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {cardNames.map((name, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{name}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
 
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {annualFees.map((fee, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{fee}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+          <h2>Annual Fee</h2>
 
-      <h2>Bonus Offer</h2>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {annualFees.map((fee, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{fee}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
 
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {bonusOffers.map((fee, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{fee}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+          <h2>Bonus Offer</h2>
 
-      <h2>Cash Back</h2>
-      
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {cashBack.map((cashback, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{cashback}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {bonusOffers.map((fee, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{fee}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
 
-      <h2>Pros</h2>
-      
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {pros.map((pros, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{pros}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+          <h2>Cash Back</h2>
 
-      <h2>Additional Info</h2>
-      
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            {more.map((moreInfo, index) => (
-              <td className={styles.column} key={index}>
-                <div className={styles.cellContent}>{moreInfo}</div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {cashBack.map((cashback, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{cashback}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+
+          <h2>Pros</h2>
+
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {pros.map((pros, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{pros}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+
+          <h2>Additional Info</h2>
+
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                {more.map((moreInfo, index) => (
+                  <td
+                    className={`${styles.column} ${
+                      index === 1 ? styles.borderTd : ""
+                    }`}
+                    key={index}
+                  >
+                    <div className={styles.cellContent}>{moreInfo}</div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
