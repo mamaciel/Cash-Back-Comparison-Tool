34a98d77cf722de10172615fa0c24b2823a9d5f5
Marcos Maciel <mamaciel2022@gmail.com>
2023-08-23 17:19:51 -0400

    Small updates

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,7 @@
 //import { useState } from 'react'
 import "./styling/App.css";
 import MainBody from "./MainBody.tsx";
-//import AnnualFee from "./Rows/AnnualFee.tsx";
 import Header from "./Header.tsx";
-//import BonusOffer from "./Rows/BonusOffer.tsx";
 
 function App() {
   return (
